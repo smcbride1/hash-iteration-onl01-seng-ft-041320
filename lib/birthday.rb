b6ed1615a4adb2f4584bb80a4d118c0e84c25b2e
Stephen McBride <stevemcbride3@gmail.com>
2020-04-16 17:49:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,13 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
+end
+
+def age_appropriate_birthday(birthday_kids)
+  # add your code snippet here!
+  birthday_kids.each do |kids_name, age|
     if age < 12
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
